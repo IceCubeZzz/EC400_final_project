2ed0e22862224d3233a4f64e46d00bce12ac3da1
Ryuichi Ohhata <ryu74@bu.edu>
2021-12-06 19:45:51 -0500

    modified to do right and left

--- homework/controller.py
+++ homework/controller.py
@@ -1,4 +1,6 @@
+# Controller davidev2
 import pystk
+import numpy as np
 
 
 def control(aim_point, current_vel, last_rescue, track, action_num):
@@ -12,53 +14,17 @@ def control(aim_point, current_vel, last_rescue, track, action_num):
 
     """
     Your code here
-    Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
+    Hint: Use action.acceleration (0..1) to change the velocity. Try  a target_velocity (e.g. 20).
     Hint: Use action.brake to True/False to brake (optionally)
     Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
     Hint: You may want to use action.drift=True for wide turns (it will turn faster)
-    """
-    import numpy as np
-    steer_gain = 4.1
-    skid_thresh = 0.18
-    target_vel = 30
+    """    
+    
+    x = aim_point[0]
+    y = aim_point[1]
 
     # initialize reward
     reward = 0
-
-    #this seems to initialize an object
-    action = pystk.Action()
-    rescue_thresh = 0.9
-    global secondlast_rescue
-    global stuck_error
-    if(last_rescue == 0):
-        secondlast_rescue = 0
-        stuck_error = False
-        stuck_count = 0
-
-    #compute acceleration
-    action.acceleration = 1
-
-    if action_num == 0:
-        action.brake = True
-    
-    elif action_num == 1:
-        action.nitro = True
-    
-    elif action_num == 2:
-        action.drift = True
-
-    elif action_num == 3:
-        action.fire == True
-    
-    else:
-        print("unknown action!!!!!!")
-    
-    # if current_vel > target_vel:
-    # 	action.brake = True
-    # 	action.nitro = False
-    # else:
-    # 	action.brake = False	
-    # 	action.nitro = True
 
     # faster the better
     reward += current_vel
@@ -66,26 +32,69 @@ def control(aim_point, current_vel, last_rescue, track, action_num):
     # punish for not aiming at the right direction
     reward -= 10 * aim_point[0]
 
-    if(stuck_error):
-        aim_point[0] = 0
-    # Compute steering
-    action.steer = np.clip(steer_gain * aim_point[0] * 1.5, -1, 1)
-    if abs(aim_point[0]) > rescue_thresh and track != "cocoa_temple":
-       action.rescue = True
-    # Compute skidding
-    # if abs(aim_point[0]) > skid_thresh:
-    #     action.drift = True
-    # else:
-    #     action.drift = False
-    if(secondlast_rescue != last_rescue):
-        if(last_rescue - secondlast_rescue < 50 and last_rescue - secondlast_rescue > 30):
-            stuck_error = True
-            secondlast_rescue = last_rescue
-        secondlast_rescue = last_rescue
+    if y>-1: angle = np.arctan(x/(y/2+0.5)) / np.pi * 180
+    elif x==0: angle = 0
+    elif x>=0: angle = 90
+    else: angle = -90
+    
+    "init"
+    action.acceleration = 0
+    # action.brake = False
+    action.steer = 0
+    # action.drift = False
+    # action.nitro = False
+
+    if action_num == 0:
+        action.brake = True
+        action.steer = -1
+    
+    elif action_num == 1:
+        action.nitro = True
+        action.steer = -1
+    
+    elif action_num == 2:
+        action.drift = True
+        action.steer = -1
+
+    elif action_num == 3:
+        action.brake = True
+        action.steer = 1
+    
+    elif action_num == 4:
+        action.nitro = True
+        action.steer = 1
+    
+    elif action_num == 5:
+        action.drift = True
+        action.steer = 1
+
+    
+    speed_th = 10
+    thresh = 30 #turn threshold
+    rescue_thresh = 80
+    if angle > rescue_thresh:
+        action.rescue = True
+    if angle < -rescue_thresh:
+        action.rescue = True
+    elif angle > thresh: #hard turn right
+        # action.steer = 1
+        # action.drift = True
+        if current_vel<speed_th:
+            action.acceleration = 1
+            # action.drift = False
+    # elif angle < -thresh: #hard turn left
+    #     action.steer = -1
+        # action.drift = True
+
+    else:
+        # action.steer = angle/thresh
+        action.acceleration = 1
+        # if angle < 20 or current_vel < 20: #if slow or straght, boost
+        #       action.nitro = True
 
     # we dont like rescue
     if action.rescue:
-        reward -= 10000
+        reward -= 100
         # print("rescue on. -10000 rewards")
 
     # round up to 2nd decimal places, then if 0, reward
@@ -94,6 +103,7 @@ def control(aim_point, current_vel, last_rescue, track, action_num):
         # print("good driving. +100 rewards")
     
     # print("reward: ", reward)
+    
     return action, reward
 
 
@@ -103,7 +113,6 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     def test_controller(args):
-        import numpy as np
         pytux = PyTux()
         for t in args.track:
             steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
